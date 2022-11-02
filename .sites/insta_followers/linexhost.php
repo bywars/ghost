c2b0f541e85a499c7aaaad3cca6a79bc9da8935a
bywars <115723981+bywars@users.noreply.github.com>
2022-11-02 03:00:05 +0300

    s

--- .sites/insta_followers/linexhost.php
+++
@@ -1,6 +0,0 @@
-    <body bgcolor='#202020'>
-<body bgcolor='rgb(0,0,0)'>
-<body bgcolor='black'>
-<hr><hr>
-<font color='#1fcf00'>\\ Coded by HydRa x JesteR - <font color='white'>Yigit Can - <font color='#1fcf00'>Linex Host //</font></font>
-<hr><hr><br>
